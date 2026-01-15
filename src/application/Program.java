81a1ee2185e995ad6b3b63feb3e2eac5ec3ac2de
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-01-15 20:08:42 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,13 +6,12 @@ import model.dao.SellerDao;
 public class Program {
 
 	public static void main(String[] args) {
-
-//		Department obj = new Department(1, "Books");
-//		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
+		
+		System.out.println("==== TEST 1: seller findById ====");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
 		System.out.println(sellerDao.findById(3));
+		
 	}
 
 }

e83e1bbbc4cff9f5a4e8ef06c8223079ab4cc315
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-01-16 22:30:01 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,11 +12,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("==== TEST 1: seller findById ====");
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println(sellerDao.findById(3));
-		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 		
 		System.out.println("\n==== TEST 2: seller findByDepartment ====");
 		Department dep = new Department(2, null);
@@ -27,10 +27,16 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
 		
-		System.out.println("\n==== TEST 4: seller findByDepartment ====");
+		System.out.println("\n==== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
-		sellerDao.insert(newSeller);
+		//sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n==== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Updated Complete!");
 		
 	}
 
